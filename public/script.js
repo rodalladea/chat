66a28c208009cee1fa5ffb5f9c9342c657946a42
Rodrigo Dalla Dea <rodrigoddea@gmail.com>
2020-03-26 03:53:20 -0300

    first commit with a simple chat

---
+++ public/script.js
@@ -0,0 +1,37 @@
+var socket = io('http://localhost:3000');
+
+function renderMessage(message) {
+  $('.messages').append(
+    `<div class="message">
+      <strong>${message.author}</strong>: ${message.message}
+    </div>`
+  );
+}
+
+socket.on('previousMessages', function(messages) {
+  for (message of messages) {
+    renderMessage(message);
+  }
+});
+
+socket.on('receivedMessage', function(message) {
+  renderMessage(message);
+});
+
+$('#chat').submit(event => {
+  event.preventDefault();
+
+  var author = $('input[name=username]').val();
+  var message = $('input[name=message]').val();
+
+  if (author.length && message.length) {
+    var messageObject = {
+      author,
+      message
+    };
+
+    renderMessage(messageObject);
+
+    socket.emit('sendMessage', messageObject);
+  }
+});
