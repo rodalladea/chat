cff9ecc0227b52d9fc2f38dd3c6dcb6e619aa372
Rodrigo Dalla Dea <rodrigoddea@gmail.com>
2020-05-05 23:15:43 -0300

    Changed the front to see a list

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ioClient from 'socket.io-client';
 
 const ENDPOINT = 'http://localhost:3333';
 const socket = ioClient(ENDPOINT);
 
 function App() {
-  const [response, setResponse] = useState('');
+  const [responses, setResponses] = useState([]);
   const [message, setMessage] = useState('');
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    socket.emit('getAllUsers');
+  }, [])
   
   socket.on('receivedMessage', data => {
-    console.log('oi');
-    setResponse(data);
+    setResponses([...responses, data]);
+  });
+
+  socket.on('allUsers', data => {
+    setUsers(data);
   });
 
   const enviar = (e, message) => {
     e.preventDefault();
+    setResponses([...responses, message]);
     socket.emit('sendMessage', message);
   }
 
@@ -24,7 +33,12 @@ function App() {
         <input type="text" onChange={e => setMessage(e.target.value)}/>
         <button type="submit">Enviar</button>
 
-        <p>{response}</p>
+        <ul>
+          {responses.map(response => <li>{response}</li>)}
+        </ul>
+        <ul>
+          {users.map(user => <li key={user}>{user}</li>)}
+        </ul>
       </form>
     </div>
   )
